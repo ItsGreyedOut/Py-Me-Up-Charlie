3b5a8d48e2eeda5938e8d09125638951f6c84616
Grey Hardy <greyhardy7@gmail.com>
2021-06-29 16:16:30 -0400

    pybank code update

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,12 @@
-# test
+# pybank python challenge
+# GT Python Homework
+
+# import add ons:  Os Library & CSV Library
+import os 
+import csv
+
+# set file location variable
+ # csvpath = "c:/path"
+
+
+
